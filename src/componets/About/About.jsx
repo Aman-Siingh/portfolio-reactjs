74adb17275928ea3cf651b25436a67e82a68020f
Pooja Singh <amansiingh15@gmail.com>
2024-06-19 00:34:01 +0530

    about section added

--- src/componets/About/About.jsx
+++ src/componets/About/About.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { getImageUrl } from '../../utils'
+import styles from "./About.module.css"
 
 export default function About() {
   return (
-    <div>About</div>
+    <section className={styles.container}>
+      <sub className={styles.subSection}>
+      <h1 className={styles.title}>ABOUT</h1>
+      <img src={getImageUrl("about/aboutImage.png")} alt="About Image" className={styles.aboutImg} />
+      <div className={styles.content}>
+          <ul className={styles.skillSection}>
+            <li className={styles.skill}>
+              <img src={getImageUrl("about/cursorIcon.png")} alt="" />
+              <h3>Front-end Developer</h3>
+              <p>I'm a Frontend Developer with expirence </p>
+            </li>
+            <li className={styles.skill}>
+              <img src={getImageUrl("about/serverIcon.png")} alt="" />
+              <h3>Back-end Developer</h3>
+              <p>I'm a Backend Developer with expirence </p>
+            </li>
+          </ul>
+      </div>
+      </sub>
+    </section>
   )
 }
