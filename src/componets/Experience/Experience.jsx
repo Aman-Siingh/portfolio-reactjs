8f3fe72fe6ff29bb4cf7b9c21a65b0bc81482437
Pooja Singh <amansiingh15@gmail.com>
2024-06-23 23:27:07 +0530

    error

--- src/componets/Experience/Experience.jsx
+++ src/componets/Experience/Experience.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import skills from "../../data/skills.json";
+import { getImageUrl } from '../../utils';
+
 
 export default function Experience() {
   return (
-    <div></div>
+    <section>
+      <div>
+        <ul>
+          {skills.map(skill, id) => {
+            return <div key={id}>
+                  <img src={getImageUrl(skills.imageSrc)} alt={skills.title}/>
+            </div>
+          }}
+        </ul>
+      </div>
+    </section>
   )
 }
